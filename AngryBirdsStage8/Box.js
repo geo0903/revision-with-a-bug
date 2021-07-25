0a5b8215b4e32615bfc34d3e156d270c9776cb17
geo0903 <82383841+geo0903@users.noreply.github.com>
2021-07-25 16:39:21 +0530

    Add files via upload

---
+++ AngryBirdsStage8/Box.js
@@ -0,0 +1,10 @@
+class Box extends BaseClass {
+  constructor(x, y, width, height){
+    super(x,y,width,height);
+  }
+  display(){
+    stroke(0,330,20)
+    super.display();
+  }
+
+};
---
+++ AngryBirdsStage8/sketch.js
@@ -0,0 +1,116 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var box1;
+var backgroundImg,platform;
+var bird, slingshot;
+
+var gameState = "onSling";
+var bg = "sprites/bg1.png";
+var score = 0;
+
+function preload() {
+    getBackgroundImg();
+}
+
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+
+
+    ground = new Ground(600,height,1200,20);
+
+    box1 = new Box(700,320,70,70);
+    box2 = new Box(920,320,70,70);
+   box3 = new Box(700,240,70,70);
+    box4 = new Box(920,240,70,70);
+    box5 = new Box(920,160,70,70);
+    box6 = new Box(700,320,70,70);
+    box7 = new Box(920,320,70,70);
+   box8= new Box(700,240,70,70);
+    box9 = new Box(920,240,70,70);
+    box0 = new Box(920,160,70,70);
+  
+
+    bird = new Bird(200,50);
+
+    //log6 = new Log(230,180,80, PI/2);
+    slingshot = new SlingShot(bird.body,{x:200, y:50});
+}
+
+function draw(){
+    if(backgroundImg)
+        background(backgroundImg);
+    console.log(bird.body.speed)
+        noStroke();
+        textSize(35)
+        fill("white")
+        text("Score  " + score, width-300, 50)
+    
+    Engine.update(engine);
+    //strokeWeight(4);
+    box1.display();
+    box2.display();
+    ground.display();
+    
+
+    box3.display();
+    box4.display();
+    box5.display();
+    box6.display();
+    box7.display();
+    box8.display();
+    box9.display();
+    box0.display();
+
+    bird.display();
+    //platform.display();
+    //log6.display();
+    slingshot.display();    
+}
+
+function mouseDragged(){
+   // if (gameState!=="launched"){
+        Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+   
+}
+
+
+function mouseReleased(){
+    slingshot.fly();
+    gameState = "launched";
+}
+
+function keyPressed(){
+    if(keyCode === 32&&bird.body.speed<1){
+        bird.trajectory=[]
+        Matter.Body.setPosition(bird.body, {
+            x:200,
+            y:50
+        })
+        slingshot.attach(bird.body);
+        
+    }
+}
+
+async function getBackgroundImg(){
+    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+    var responseJSON = await response.json();
+
+    var datetime = responseJSON.datetime;
+    var hour = datetime.slice(11,13);
+    
+    if(hour>=06 && hour<=19){
+        bg = "sprites/bg1.png";
+    }
+    else{
+        bg = "sprites/bg2.jpg";
+    }
+
+    backgroundImg = loadImage(bg);
+    console.log(backgroundImg);
+}
